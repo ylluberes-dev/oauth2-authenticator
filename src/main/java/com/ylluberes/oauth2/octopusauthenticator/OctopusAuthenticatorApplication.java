1273164ecbea7b54d6c24fb65271813bf28202bc
ylluberes-dev <ylluberes19@gmail.com>
2021-09-06 00:27:31 -0800

    Initial commit

---
+++ src/main/java/com/ylluberes/oauth2/octopusauthenticator/OctopusAuthenticatorApplication.java
@@ -0,0 +1,13 @@
+package com.ylluberes.oauth2.octopusauthenticator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OctopusAuthenticatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OctopusAuthenticatorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ylluberes/oauth2/octopusauthenticator/OctopusAuthenticatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ylluberes.oauth2.octopusauthenticator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OctopusAuthenticatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
